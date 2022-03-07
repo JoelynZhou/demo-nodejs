04a14f822389275c1cf887d65294c54fa73e6951
joelyn.zhou <joelyn.zhou@bybit.com>
2022-03-07 12:46:41 +0800

    feat: 常用 api

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(Date);
+console.log(Math);
+
+console.log(setTimeout);
+console.log(setInterval);
+// console.log(requestAnimationFrame); // is not defined 原因：不是处在浏览器环境，作为弥补，nodejs 提供了 setImmediate 函数
+console.log(setImmediate);
+
+console.log(__filename); //当前运行的脚本所在的位置
+console.log(__dirname); //当前运行的脚本所在的目录位置
+
+console.log(process); //运行 nodejs 的进程信息
